6281647b2687cf6fe8a66654766c54d3735c6df7
Moises Silva <msilva@sangoma.com>
2016-08-09 03:34:31 -0400

    Add inbound/outbound session count and outbound ASR

--- mod_prometheus.rs
+++ mod_prometheus.rs
@@ -1,3 +1,9 @@
+// TODO:
+// - Initialize counters/gauges to current values on module load
+//   using switch_core_session_count(), switch_core_session_ctl() etc
+// - Make bindaddr configurable
+// - Allow configuring metrics that can be later references the dialplan
+// - Add dimensions to metrics (e.g inbound per profile)
 #[macro_use]
 extern crate lazy_static;
 
@@ -7,7 +13,7 @@ extern crate prometheus;
 extern crate libc;
 
 use std::sync::{Arc, Mutex};
-//use std::borrow::Cow;
+use std::borrow::Cow;
 
 use freeswitchrs::raw as fsr;
 use freeswitchrs::mods::*; // This will get replaced with a mods prelude
@@ -28,9 +34,33 @@ lazy_static! {
         Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_session_count".to_string(),
                                                      "FreeSWITCH Session Count".to_string())))
     };
+    static ref METRIC_SESSIONS_INBOUND: Arc<Mutex<Counter>> = {
+        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_inbound".to_string(),
+                                                   "FreeSWITCH Inbound Sessions Count".to_string())))
+    };
+    static ref METRIC_SESSIONS_INBOUND_ANSWERED: Arc<Mutex<Counter>> = {
+        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_inbound_answered".to_string(),
+                                                   "FreeSWITCH Answered Inbound Sessions Count".to_string())))
+    };
+    static ref METRIC_SESSIONS_OUTBOUND: Arc<Mutex<Counter>> = {
+        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_outbound".to_string(),
+                                                   "FreeSWITCH Outbound Sessions Count".to_string())))
+    };
+    static ref METRIC_SESSIONS_OUTBOUND_ANSWERED: Arc<Mutex<Counter>> = {
+        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_outbound_answered".to_string(),
+                                                   "FreeSWITCH Answered Outbound Sessions Count".to_string())))
+    };
+    static ref METRIC_SESSIONS_ANSWERED: Arc<Mutex<Counter>> = {
+        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_answered".to_string(),
+                                                   "FreeSWITCH Answered Sessions Count".to_string())))
+    };
     static ref METRIC_SESSIONS_ACTIVE: Arc<Mutex<Gauge>> = {
         Arc::new(Mutex::new(prometheus::Gauge::new("freeswitch_sessions_active".to_string(),
                                                    "FreeSWITCH Active Sessions".to_string())))
+    };
+    static ref METRIC_SESSIONS_ASR: Arc<Mutex<Gauge>> = {
+        Arc::new(Mutex::new(prometheus::Gauge::new("freeswitch_sessions_asr".to_string(),
+                                                   "FreeSWITCH Answer Seizure Ratio".to_string())))
     };
 }
 
@@ -46,30 +76,47 @@ fn prometheus_load(mod_int: &ModInterface) -> Status {
         let mut r = reg.lock().unwrap();
         r.register_counter((*METRIC_HEARTBEAT_COUNT).clone());
         r.register_counter((*METRIC_SESSION_COUNT).clone());
+        r.register_counter((*METRIC_SESSIONS_INBOUND).clone());
+        r.register_counter((*METRIC_SESSIONS_INBOUND_ANSWERED).clone());
+        r.register_counter((*METRIC_SESSIONS_OUTBOUND).clone());
+        r.register_counter((*METRIC_SESSIONS_OUTBOUND_ANSWERED).clone());
+        r.register_counter((*METRIC_SESSIONS_ANSWERED).clone());
         r.register_gauge((*METRIC_SESSIONS_ACTIVE).clone());
+        r.register_gauge((*METRIC_SESSIONS_ASR).clone());
     }
 
-    freeswitchrs::event_bind("mod_prometheus", fsr::event_types::HEARTBEAT, None, |e| {
-        unsafe {
-            if let Some(sesscount) = e.header("Session-Count") {
-                fslog!(WARNING, "Received heartbeat with Session-Count: {}", sesscount);
-                METRIC_HEARTBEAT_COUNT.lock().unwrap().increment();
-            } else {
-                fslog!(WARNING, "Received heartbeat without Session-Count header {}", "");
-            }
-        }
+    freeswitchrs::event_bind("mod_prometheus", fsr::event_types::HEARTBEAT, None, |_| {
+        METRIC_HEARTBEAT_COUNT.lock().unwrap().increment();
     });
-    freeswitchrs::event_bind("mod_prometheus", fsr::event_types::CHANNEL_CREATE, None, |_| {
-        unsafe {
-            fslog!(WARNING, "Received channel create{}", "");
-            METRIC_SESSION_COUNT.lock().unwrap().increment();
-            METRIC_SESSIONS_ACTIVE.lock().unwrap().increment();
+    freeswitchrs::event_bind("mod_prometheus", fsr::event_types::CHANNEL_CREATE, None, |e| {
+        METRIC_SESSION_COUNT.lock().unwrap().increment();
+        METRIC_SESSIONS_ACTIVE.lock().unwrap().increment();
+        if let Some(direction) = e.header("Call-Direction") {
+            if direction == "inbound" {
+                METRIC_SESSIONS_INBOUND.lock().unwrap().increment();
+            } else {
+                let outbound = METRIC_SESSIONS_OUTBOUND.lock().unwrap().increment();
+                let asr = METRIC_SESSIONS_OUTBOUND_ANSWERED.lock().unwrap().value() / outbound;
+                METRIC_SESSIONS_ASR.lock().unwrap().set(asr);
+            }
+        } else {
+            let b = e.body().unwrap_or(Cow::Borrowed("<No Body>"));
+            unsafe { fslog!(WARNING, "Received channel create event with no call direction: {:?}\n", b); }
         }
     });
     freeswitchrs::event_bind("mod_prometheus", fsr::event_types::CHANNEL_DESTROY, None, |_| {
-        unsafe {
-            fslog!(WARNING, "Received channel create{}", "");
-            METRIC_SESSIONS_ACTIVE.lock().unwrap().decrement();
+        METRIC_SESSIONS_ACTIVE.lock().unwrap().decrement();
+    });
+    freeswitchrs::event_bind("mod_prometheus", fsr::event_types::CHANNEL_ANSWER, None, |e| {
+        METRIC_SESSIONS_ANSWERED.lock().unwrap().increment();
+        if let Some(direction) = e.header("Call-Direction") {
+            if direction == "inbound" {
+                METRIC_SESSIONS_INBOUND_ANSWERED.lock().unwrap().increment();
+            } else {
+                let answered = METRIC_SESSIONS_OUTBOUND_ANSWERED.lock().unwrap().increment();
+                let asr = answered / METRIC_SESSIONS_OUTBOUND.lock().unwrap().value();
+                METRIC_SESSIONS_ASR.lock().unwrap().set(asr);
+            }
         }
     });
     unsafe {
