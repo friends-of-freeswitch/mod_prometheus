5de92ef98c9ef98395593192a7282a45a42f273e
Moises Silva <msilva@sangoma.com>
2016-08-11 00:12:54 -0400

    Refactor metric registration to use static lists of metrics

--- mod_prometheus.rs
+++ mod_prometheus.rs
@@ -17,6 +17,8 @@ extern crate libc;
 
 use std::sync::{Arc, Mutex};
 use std::borrow::Cow;
+use std::collections::HashMap;
+use std::ops::Index;
 
 use freeswitchrs::raw as fsr;
 use freeswitchrs::mods::*; // This will get replaced with a mods prelude
@@ -25,126 +27,134 @@ use freeswitchrs::raw::log_level::{INFO, WARNING};
 
 use prometheus::{Registry, Counter, Gauge};
 
+enum FSCounter {
+    Heartbeats = 0,
+    Sessions,
+    SessionsAnswered,
+    SessionsFailed,
+    SessionsInbound,
+    SessionsInboundAnswered,
+    SessionsInboundFailed,
+    SessionsOutbound,
+    SessionsOutboundAnswered,
+    SessionsOutboundFailed,
+    Registrations,
+    RegistrationAttempts,
+    RegistrationFailures
+}
+
+enum FSGauge {
+    SessionsActive,
+    SessionsASR,
+    RegistrationsActive
+}
+
 lazy_static! {
     static ref REGISTRY: Arc<Mutex<Registry>> = {
-        Arc::new(Mutex::new(prometheus::Registry::new("0.0.0.0".to_string(), 6780)))
+        Arc::new(Mutex::new(Registry::new("0.0.0.0".to_string(), 6780)))
     };
-    static ref METRIC_HEARTBEAT_COUNT: Arc<Mutex<Counter>> = {
-        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_heartbeat_count".to_string(),
-                                                     "FreeSWITCH heartbeat count".to_string())))
+    static ref USER_COUNTERS: HashMap<String, Arc<Mutex<Counter>>> = {
+        HashMap::new()
     };
-    static ref METRIC_SESSION_COUNT: Arc<Mutex<Counter>> = {
-        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_session_count".to_string(),
-                                                     "FreeSWITCH Session Count".to_string())))
+    static ref USER_GAUGES: HashMap<String, Arc<Mutex<Gauge>>> = {
+        HashMap::new()
     };
-    static ref METRIC_SESSIONS_ANSWERED: Arc<Mutex<Counter>> = {
-        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_answered".to_string(),
-                                                   "FreeSWITCH Answered Sessions Count".to_string())))
-    };
-    static ref METRIC_SESSIONS_FAILED: Arc<Mutex<Counter>> = {
-        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_failed".to_string(),
-                                                     "FreeSWITCH Failed Sessions Count".to_string())))
-    };
-    static ref METRIC_SESSIONS_INBOUND: Arc<Mutex<Counter>> = {
-        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_inbound".to_string(),
-                                                   "FreeSWITCH Inbound Sessions Count".to_string())))
-    };
-    static ref METRIC_SESSIONS_INBOUND_ANSWERED: Arc<Mutex<Counter>> = {
-        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_inbound_answered".to_string(),
-                                                   "FreeSWITCH Answered Inbound Sessions Count".to_string())))
-    };
-    static ref METRIC_SESSIONS_INBOUND_FAILED: Arc<Mutex<Counter>> = {
-        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_inbound_failed".to_string(),
-                                                   "FreeSWITCH Failed Inbound Sessions Count".to_string())))
-    };
-    static ref METRIC_SESSIONS_OUTBOUND: Arc<Mutex<Counter>> = {
-        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_outbound".to_string(),
-                                                   "FreeSWITCH Outbound Sessions Count".to_string())))
-    };
-    static ref METRIC_SESSIONS_OUTBOUND_ANSWERED: Arc<Mutex<Counter>> = {
-        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_outbound_answered".to_string(),
-                                                   "FreeSWITCH Answered Outbound Sessions Count".to_string())))
-    };
-    static ref METRIC_SESSIONS_OUTBOUND_FAILED : Arc<Mutex<Counter>> = {
-        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_outbound_failed".to_string(),
-                                                   "FreeSWITCH Failed Outbound Sessions Count".to_string())))
-    };
-    static ref METRIC_SESSIONS_ACTIVE: Arc<Mutex<Gauge>> = {
-        Arc::new(Mutex::new(prometheus::Gauge::new("freeswitch_sessions_active".to_string(),
-                                                   "FreeSWITCH Active Sessions".to_string())))
-    };
-    static ref METRIC_SESSIONS_ASR: Arc<Mutex<Gauge>> = {
-        Arc::new(Mutex::new(prometheus::Gauge::new("freeswitch_sessions_asr".to_string(),
-                                                   "FreeSWITCH Answer Seizure Ratio".to_string())))
-    };
+    static ref COUNTERS: [Arc<Mutex<Counter>>;13] = {[
+        Arc::new(Mutex::new(Counter::new("freeswitch_heartbeats".to_string(),
+                                         "FreeSWITCH heartbeat count".to_string()))),
 
-    // Registration Metrics
-    static ref METRIC_REGISTRATION_COUNT: Arc<Mutex<Counter>> = {
-        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_registration_count".to_string(),
-                                                   "FreeSWITCH Registration Count".to_string())))
-    };
-    static ref METRIC_REGISTRATION_ATTEMPTS: Arc<Mutex<Counter>> = {
+        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions".to_string(),
+                                                     "FreeSWITCH Session Count".to_string()))),
+
+        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_answered".to_string(),
+                                                     "FreeSWITCH Answered Sessions Count".to_string()))),
+
+        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_failed".to_string(),
+                                                     "FreeSWITCH Failed Sessions Count".to_string()))),
+
+        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_inbound".to_string(),
+                                                     "FreeSWITCH Inbound Sessions Count".to_string()))),
+
+        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_inbound_answered".to_string(),
+                                                     "FreeSWITCH Answered Inbound Sessions Count".to_string()))),
+
+        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_inbound_failed".to_string(),
+                                                     "FreeSWITCH Failed Inbound Sessions Count".to_string()))),
+
+        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_outbound".to_string(),
+                                                     "FreeSWITCH Outbound Sessions Count".to_string()))),
+
+        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_outbound_answered".to_string(),
+                                                     "FreeSWITCH Answered Outbound Sessions Count".to_string()))),
+
+        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_sessions_outbound_failed".to_string(),
+                                                     "FreeSWITCH Failed Outbound Sessions Count".to_string()))),
+
+        // Registration Metrics
+        Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_registrations".to_string(),
+                                                     "FreeSWITCH Registration Count".to_string()))),
+
         Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_registration_attempts".to_string(),
-                                                   "FreeSWITCH Registration Attempts".to_string())))
-    };
-    static ref METRIC_REGISTRATION_FAILURES: Arc<Mutex<Counter>> = {
+                                                     "FreeSWITCH Registration Attempts".to_string()))),
+
         Arc::new(Mutex::new(prometheus::Counter::new("freeswitch_registration_failures".to_string(),
-                                                   "FreeSWITCH Registration Failures".to_string())))
-    };
-    static ref METRIC_REGISTRATIONS_ACTIVE: Arc<Mutex<Gauge>> = {
+                                                     "FreeSWITCH Registration Failures".to_string())))
+    ]};
+    static ref GAUGES: [Arc<Mutex<Gauge>>;3] = {[
+        Arc::new(Mutex::new(prometheus::Gauge::new("freeswitch_sessions_active".to_string(),
+                                                   "FreeSWITCH Active Sessions".to_string()))),
+
+        Arc::new(Mutex::new(prometheus::Gauge::new("freeswitch_sessions_asr".to_string(),
+                                                   "FreeSWITCH Sessions Answer Seizure Ratio".to_string()))),
+
         Arc::new(Mutex::new(prometheus::Gauge::new("freeswitch_registrations_active".to_string(),
                                                    "FreeSWITCH Active Registrations".to_string())))
-    };
+    ]};
+}
+
+impl Index<FSCounter> for [Arc<Mutex<Counter>>] {
+    type Output = Arc<Mutex<Counter>>;
+    fn index(&self, idx: FSCounter) -> &Arc<Mutex<Counter>> {
+        &self[idx as usize]
+    }
+}
+
+impl Index<FSGauge> for [Arc<Mutex<Gauge>>] {
+    type Output = Arc<Mutex<Gauge>>;
+    fn index(&self, idx: FSGauge) -> &Arc<Mutex<Gauge>> {
+        &self[idx as usize]
+    }
 }
 
 fn prometheus_load(mod_int: &ModInterface) -> Status {
-    mod_int.add_raw_api("counter_increase", "Increase counter", "counter_increase", counter_increase_api);
-
     let ref reg = *REGISTRY;
-
     // At some point we'll have to configure things ...
     //let xml = fsr::xml_open_cfg();
     Registry::start(&reg);
     {
         let mut r = reg.lock().unwrap();
-
-        // Session metrics
-        r.register_counter((*METRIC_HEARTBEAT_COUNT).clone());
-        r.register_counter((*METRIC_SESSION_COUNT).clone());
-        r.register_counter((*METRIC_SESSIONS_FAILED).clone());
-        r.register_counter((*METRIC_SESSIONS_ANSWERED).clone());
-        r.register_counter((*METRIC_SESSIONS_INBOUND).clone());
-        r.register_counter((*METRIC_SESSIONS_INBOUND_ANSWERED).clone());
-        r.register_counter((*METRIC_SESSIONS_INBOUND_FAILED).clone());
-        r.register_counter((*METRIC_SESSIONS_OUTBOUND).clone());
-        r.register_counter((*METRIC_SESSIONS_OUTBOUND_ANSWERED).clone());
-        r.register_counter((*METRIC_SESSIONS_OUTBOUND_FAILED).clone());
-        r.register_gauge((*METRIC_SESSIONS_ACTIVE).clone());
-        r.register_gauge((*METRIC_SESSIONS_ASR).clone());
-
-        // Registration metrics
-        r.register_counter((*METRIC_REGISTRATION_COUNT).clone());
-        r.register_counter((*METRIC_REGISTRATION_ATTEMPTS).clone());
-        r.register_counter((*METRIC_REGISTRATION_FAILURES).clone());
-        r.register_gauge((*METRIC_REGISTRATIONS_ACTIVE).clone());
+        for c in COUNTERS.iter() {
+            r.register_counter(c.clone());
+        }
+        for g in GAUGES.iter() {
+            r.register_gauge(g.clone());
+        }
     }
-
     // Heartbeat counts
     freeswitchrs::event_bind("mod_prometheus", fsr::event_types::HEARTBEAT, None, |_| {
-        METRIC_HEARTBEAT_COUNT.lock().unwrap().increment();
+        COUNTERS[FSCounter::Heartbeats].lock().unwrap().increment();
     });
-
     // New channel created
     freeswitchrs::event_bind("mod_prometheus", fsr::event_types::CHANNEL_CREATE, None, |e| {
-        METRIC_SESSION_COUNT.lock().unwrap().increment();
-        METRIC_SESSIONS_ACTIVE.lock().unwrap().increment();
+        COUNTERS[FSCounter::Sessions].lock().unwrap().increment();
+        GAUGES[FSGauge::SessionsActive].lock().unwrap().increment();
         if let Some(direction) = e.header("Call-Direction") {
             if direction == "inbound" {
-                METRIC_SESSIONS_INBOUND.lock().unwrap().increment();
+                COUNTERS[FSCounter::SessionsInbound].lock().unwrap().increment();
             } else {
-                let outbound = METRIC_SESSIONS_OUTBOUND.lock().unwrap().increment();
-                let asr = METRIC_SESSIONS_OUTBOUND_ANSWERED.lock().unwrap().value() / outbound;
-                METRIC_SESSIONS_ASR.lock().unwrap().set(asr);
+                let outbound = COUNTERS[FSCounter::SessionsOutbound].lock().unwrap().increment();
+                let asr = COUNTERS[FSCounter::SessionsOutboundAnswered].lock().unwrap().value() / outbound;
+                GAUGES[FSGauge::SessionsASR].lock().unwrap().set(asr);
             }
         } else {
             let b = e.body().unwrap_or(Cow::Borrowed("<No Body>"));
@@ -154,14 +164,14 @@ fn prometheus_load(mod_int: &ModInterface) -> Status {
 
     // Channel answered
     freeswitchrs::event_bind("mod_prometheus", fsr::event_types::CHANNEL_ANSWER, None, |e| {
-        METRIC_SESSIONS_ANSWERED.lock().unwrap().increment();
+        COUNTERS[FSCounter::SessionsAnswered].lock().unwrap().increment();
         if let Some(direction) = e.header("Call-Direction") {
             if direction == "inbound" {
-                METRIC_SESSIONS_INBOUND_ANSWERED.lock().unwrap().increment();
+                COUNTERS[FSCounter::SessionsInboundAnswered].lock().unwrap().increment();
             } else {
-                let answered = METRIC_SESSIONS_OUTBOUND_ANSWERED.lock().unwrap().increment();
-                let asr = answered / METRIC_SESSIONS_OUTBOUND.lock().unwrap().value();
-                METRIC_SESSIONS_ASR.lock().unwrap().set(asr);
+                let answered = COUNTERS[FSCounter::SessionsOutboundAnswered].lock().unwrap().increment();
+                let asr = answered / COUNTERS[FSCounter::SessionsOutbound].lock().unwrap().value();
+                GAUGES[FSGauge::SessionsASR].lock().unwrap().set(asr);
             }
         } else {
             let b = e.body().unwrap_or(Cow::Borrowed("<No Body>"));
@@ -176,11 +186,11 @@ fn prometheus_load(mod_int: &ModInterface) -> Status {
             if parsed_time.is_ok() && parsed_time.unwrap() == 0 as i64 {
                 if let Some(direction) = e.header("Call-Direction") {
                     if direction == "inbound" {
-                        METRIC_SESSIONS_INBOUND_FAILED.lock().unwrap().increment();
+                        COUNTERS[FSCounter::SessionsInboundFailed].lock().unwrap().increment();
                     } else {
-                        METRIC_SESSIONS_OUTBOUND_FAILED.lock().unwrap().increment();
+                        COUNTERS[FSCounter::SessionsInboundFailed].lock().unwrap().increment();
                     }
-                    METRIC_SESSIONS_FAILED.lock().unwrap().increment();
+                    COUNTERS[FSCounter::SessionsFailed].lock().unwrap().increment();
                 } else {
                     let b = e.body().unwrap_or(Cow::Borrowed("<No Body>"));
                     unsafe { fslog!(WARNING, "Received channel hangup event with no call direction: {:?}\n", b); }
@@ -194,7 +204,7 @@ fn prometheus_load(mod_int: &ModInterface) -> Status {
 
     // Channel destroyed
     freeswitchrs::event_bind("mod_prometheus", fsr::event_types::CHANNEL_DESTROY, None, |_| {
-        METRIC_SESSIONS_ACTIVE.lock().unwrap().decrement();
+        GAUGES[FSGauge::SessionsActive].lock().unwrap().decrement();
     });
 
     // FIXME: Registrations are bound to be outdated on restart (registrations are in the db)
@@ -202,27 +212,27 @@ fn prometheus_load(mod_int: &ModInterface) -> Status {
 
     // Registration attempts
     freeswitchrs::event_bind("mod_prometheus", fsr::event_types::CUSTOM, Some("sofia::register_attempt"), |_| {
-        METRIC_REGISTRATION_ATTEMPTS.lock().unwrap().increment();
+        COUNTERS[FSCounter::RegistrationAttempts].lock().unwrap().increment();
     });
 
     // Registration failures
     freeswitchrs::event_bind("mod_prometheus", fsr::event_types::CUSTOM, Some("sofia::register_failure"), |_| {
-        METRIC_REGISTRATION_FAILURES.lock().unwrap().increment();
+        COUNTERS[FSCounter::RegistrationFailures].lock().unwrap().increment();
     });
 
     // Registration counters
     freeswitchrs::event_bind("mod_prometheus", fsr::event_types::CUSTOM, Some("sofia::register"), |_| {
-        METRIC_REGISTRATION_COUNT.lock().unwrap().increment();
-        METRIC_REGISTRATIONS_ACTIVE.lock().unwrap().increment();
+        COUNTERS[FSCounter::Registrations].lock().unwrap().increment();
+        GAUGES[FSGauge::RegistrationsActive].lock().unwrap().increment();
     });
     freeswitchrs::event_bind("mod_prometheus", fsr::event_types::CUSTOM, Some("sofia::unregister"), |_| {
-        METRIC_REGISTRATIONS_ACTIVE.lock().unwrap().decrement();
+        GAUGES[FSGauge::RegistrationsActive].lock().unwrap().decrement();
     });
     freeswitchrs::event_bind("mod_prometheus", fsr::event_types::CUSTOM, Some("sofia::expire"), |_| {
-        METRIC_REGISTRATIONS_ACTIVE.lock().unwrap().decrement();
+        GAUGES[FSGauge::RegistrationsActive].lock().unwrap().decrement();
     });
 
-
+    mod_int.add_raw_api("counter_increase", "Increase counter", "counter_increase", counter_increase_api);
     unsafe {
         fslog!(INFO, "Loaded Prometheus Metrics Module{}", "");
     }
